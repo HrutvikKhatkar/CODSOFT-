2c5eb43448797c565db923838f548eb3e36d0901
HrutvikKhatkar <127831047+hrutvik-khatkar@users.noreply.github.com>
2023-12-15 12:58:49 +0530

    updated code

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
-def task_operation(task_list,n):
-    sr_number = 1
-    if(n==1):
+def task_operation(task_list,choice):# arguments taken
+    sr_number = 1 # to maintain serial number of task in list 
+    if(choice==1):
         print("\nEnter no of task you want to add: ")
         n_of_task = int(input())
         sr_number = len(task_list) + sr_number
@@ -9,13 +9,13 @@ def task_operation(task_list,n):
             task_list+=[str(sr_number)+" "+ input()]   
             sr_number+=1
         print(len(task_list))
-    elif(n==2):
+    elif(choice==2):
         updating_at_index =int(input("\nEnter serial number of task to update: "))
         task_list[updating_at_index-1] =str(updating_at_index) + " " + input("\nEnter updated task: ")
         print("\nTask list is updated.\n")
-    elif(n==3):
+    elif(choice==3):
         display_tasks(task_list)
-    elif(n==4):
+    elif(choice==4):
         print("Program Exit........")
         exit()
     else:
@@ -28,11 +28,11 @@ def display_tasks(task_list):
         print(task)
     print("*" * 20)
 
-task_list = []
+task_list = [] #Empty list created
 while True:
     print("\nEnter 1 to add task")
     print("Enter 2 to update task")
     print("Enter 3 to display list of task")
     print("Enter 4 to Exit")
-    n = int(input("\nEnter your choice: "))
-    task_operation(task_list,n)
+    choice = int(input("\nEnter your choice: "))
+    task_operation(task_list,choice)
